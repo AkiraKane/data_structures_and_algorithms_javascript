b445c3d644e5735d7851d4d42c3680e350802c89
Ming Huang <mhuang@ip-10-200-1-96.ec2.internal>
2020-02-24 16:25:57 -0800

    add sumZero

---
+++ sumZero.js
@@ -0,0 +1,20 @@
+// Time complexity - o(n)
+// Space complexity - o(1)
+
+function sumZero(arr) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+}
+
+console.log(sumZero([-4, -3, -2, 0, 1, 2, 3, 10]));
