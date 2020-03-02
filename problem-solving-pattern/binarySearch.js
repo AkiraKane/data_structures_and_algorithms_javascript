c2497746f62e7ae17317e56f2a7094d2dfcacd8f
Ming Huang <mhuang@Mings-MacBook-Pro.local>
2020-03-02 12:54:23 -0800

    add more codes

--- problem-solving-pattern/binarySearch.js
+++ problem-solving-pattern/binarySearch.js
@@ -8,12 +8,14 @@ function binarySearch(arr, val) {
     let middle = Math.floor((min + max) / 2);
     let currentElement = arr[middle];
     if (currentElement > val) {
-      max = currentElement - 1;
+      max = middle - 1;
     } else if (currentElement < val) {
-      min = currentElement + 1;
+      min = middle + 1;
     } else {
       return middle;
     }
   }
   return -1;
 }
+
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15));
